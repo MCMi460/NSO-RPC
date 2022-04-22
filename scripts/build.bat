7c30e44ace2e3f5cc9d038b622cad6c45fc87065
Deltaion Lee <32529306+MCMi460@users.noreply.github.com>
2022-04-21 20:16:44 -0500

    Dumb bug

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,4 +1,4 @@
 cd ..\client
 python -m pip install -r requirements.txt pyinstaller
-pyinstaller --onefile --clean --noconsole --add-data "*.png;." --icon=icon.ico app.py
+python -m PyInstaller --onefile --clean --noconsole --add-data "*.png;." --icon=icon.ico app.py
 start dist
