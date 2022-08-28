467e599e09b5082569339a34195772001e9657ab
Deltaion Lee <32529306+MCMi460@users.noreply.github.com>
2022-08-28 02:11:19 -0500

    Fix app naming scheme for building on macOS
    And possibly optimize future builds.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,6 @@
 cd ../client
 python3 -m pip install -r requirements.txt py2app
 py2applet --make-setup app.py icon.icns "icon.png" "taskbarDark.png" "taskbarLight.png"
-python3 setup.py py2app
+sed -i '' -e "s/)/    name='NSO-RPC')/" setup.py
+python3 setup.py py2app -O2
 open dist
