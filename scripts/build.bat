1e3915491ad3a49dcdc275b3d370265453647f0d
Deltaion Lee <32529306+MCMi460@users.noreply.github.com>
2022-08-31 18:19:34 -0500

    Add app name to `build.bat`

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,4 +1,4 @@
 cd ..\client
 python -m pip install -r requirements.txt pyinstaller
-python -m PyInstaller --onefile --clean --noconsole --add-data "*.png;." --icon=icon.ico app.py
+python -m PyInstaller --onefile --clean --noconsole --add-data "*.png;." --icon=icon.ico --name=NSO-RPC app.py
 start dist
