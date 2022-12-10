e20328200eef0d54181f2d647cc03549c548d796
Vincent Liu <pkchu99@gmail.com>
2022-12-10 17:20:58 +1100

    fix regex for detecting version number (#55)

--- client/api/__init__.py
+++ client/api/__init__.py
@@ -44,7 +44,7 @@ def getVersion():
             break
         except:
             log('Failed to get Apple\'s store page. Retrying...')
-    searchPattern = re.compile(r'Version\s(\d\.\d\.\d)*')
+    searchPattern = re.compile(r'Version\s*(\d\.\d\.\d)+')
     version = searchPattern.findall(r.text)
     if not version:
         return ''
