574c6a9a94bcaa98c1eb850ef6e3a52335cb4081
Phoenix / Hotaru <DeriousHD@gmail.com>
2023-06-23 20:01:47 +0100

    Fix bad layout loading on edgecases

--- client/layout/__init__.py
+++ client/layout/__init__.py
@@ -1,4 +1,15 @@
+import sys
+from qtpy.QtCore import qVersion
+
 try:
-    from .qt6_layout import *
-except ImportError:
-    from .qt5_layout import *
+    qt_version = qVersion()
+    if qt_version.startswith('6'):
+        from .qt6_layout import *
+    elif qt_version.startswith('5'):
+        from .qt5_layout import *
+    else:
+        print(f"Unsupported Qt version: {qt_version}")
+        sys.exit(1)
+except (ImportError, TypeError) as e:
+    print(f"Error importing Qt modules: {e}")
+    sys.exit(1)
