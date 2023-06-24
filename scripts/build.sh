668cf1912b055f54ed6e1647c13c8798add7e67f
DarthNihilus1 <mgracer48@yahoo.com>
2023-06-23 23:52:57 -0400

    build: build a universal2 binary
    This improves performance and battery life on apple silicon macs as it doesn't need to emulate x86_64, also it still is compatible with intel macs

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,5 +2,8 @@ cd ../client
 python3 -m pip install -r requirements.txt py2app
 py2applet --make-setup app.py icon.icns "icon.png" "taskbarDark.png" "taskbarLight.png"
 sed -i '' -e "s/)/    name='NSO-RPC')/" setup.py
-python3 setup.py py2app -O2
+# build universal binary
+python3 setup.py py2app -O2 --arch=universal2
+# arm64 requires codesigning to run
+codesign --deep --force --sign - dist/NSO-RPC.app/Contents/MacOS/*
 open dist
