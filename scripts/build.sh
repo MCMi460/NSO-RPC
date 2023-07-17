dc2a3a23432cb42500412931601ba418560e5260
Phoenix / Hotaru <DeriousHD@gmail.com>
2023-07-17 11:16:39 +0100

    Force x86_64 on non-universal2 builds

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,5 +2,5 @@ cd ../client
 python3 -m pip install -r requirements.txt py2app
 py2applet --make-setup app.py icon.icns "icon.png" "taskbarDark.png" "taskbarLight.png"
 sed -i '' -e "s/)/    name='NSO-RPC')/" setup.py
-python3 setup.py py2app -O2
+python3 setup.py py2app --arch=x86_64 -O2
 open dist
