cd954d59111f091d3551c2f2f6011901e578c006
Marcus Jones <marcusjones8119@gmail.com>
2024-02-25 23:09:52 -0500

    Update Qt5 and Qt6 layout files
    Adds back the `if __name__ == "__main__"` section at the bottom of the file that didn't get generated because I forgot to use the `-x` option when running `pyuic5` and `pyuic6`

--- client/layout/qt5_layout.py
+++ client/layout/qt5_layout.py
@@ -253,3 +253,13 @@ class Ui_MainWindow(object):
         self.label_23.setText(_translate("MainWindow", "Show PFP on Discord:"))
         self.friendCodeButton.setText(_translate("MainWindow", "Set friend code"))
         self.label_24.setText(_translate("MainWindow", "Show eShop link on Discord:"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QWidget()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
