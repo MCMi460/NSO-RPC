bc4a59a7859d89ecc4d65d15e3b2fd271f02c3d6
Phoenix / Hotaru <DeriousHD@gmail.com>
2024-05-14 02:50:35 +0100

    [Fix] Update _version.py to handle tags and PR's correctly

--- client/_version.py
+++ client/_version.py
@@ -1,15 +1,25 @@
 import os
 import git
 
+
+def get_version_info():
+    r = git.repo.Repo(search_parent_directories=True)
+    try:
+        # Get all tags sorted by commit date
+        tags = sorted(r.tags, key=lambda t: t.commit.committed_datetime, reverse=True)
+        version_info = tags[0].name
+    except IndexError:
+        try:
+            version_info = r.active_branch.name
+        except TypeError:
+            version_info = r.head.object.hexsha[:7]
+    return version_info
+
+
 if __name__ == '__main__':
-    # Deletes current version.txt file
     if os.path.exists('version.txt'):
         os.remove('version.txt')
 
-    # Writes latest git version info to 'version.txt'
-    r = git.repo.Repo(search_parent_directories=True)
-    version_info = r.git.describe('--tags')
+    version_info = get_version_info()
     with open('version.txt', 'w') as f:
         f.write(version_info)
-        f.write('\n')
-        f.close()
